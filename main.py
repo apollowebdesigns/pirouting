0a0c9cee2053cb74a138d97ffea3ccbbd87ebcbd
Andy Evans <andrewevans0@outlook.com>
2016-11-20 15:13:30 +0000

    route place to main page

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import requests
 from flask import Flask
 app = Flask(__name__)
 
+ipaddress = 'http://192.168.1.69:9876'
+
 
 @app.route('/')
-def test():
-    return "hello world"
-
-@app.route('/some-url')
 def get_data():
-    return requests.get('http://example.com').content
+    return requests.get(ipaddress).content
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8888, debug=True)
