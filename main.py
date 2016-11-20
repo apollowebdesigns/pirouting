c23f002f9c0a794c2554b98e96617f7143a0bb33
Andy Evans <andrewevans0@outlook.com>
2016-11-20 15:07:42 +0000

    server started

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ def test():
 @app.route('/some-url')
 def get_data():
     return requests.get('http://example.com').content
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=9876, debug=True)
