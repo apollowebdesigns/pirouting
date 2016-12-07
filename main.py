a1f660fcb72643c7b135b764b5a84fcc385ef4c7
Andy Evans <andrewevans0@outlook.com>
2016-12-07 19:17:19 +0000

    routes added for the motor

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ app = Flask(__name__)
 ipaddress = 'http://192.168.1.69:9876'
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def get_data():
     return requests.get(ipaddress).content
+
+fowards = '/forwards'
+@app.route(fowards, methods=['GET'])
+def move_forwards():
+    return requests.get(ipaddress + fowards).content
+
+motor = '/hits/motor'
+@app.route(motor, methods=['GET'])
+def move_forwards():
+    return requests.get(ipaddress + motor).content
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8899, debug=True)
