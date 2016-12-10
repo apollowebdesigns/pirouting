1888aa2708d1598292d2c7146fa1314220cc5620
Andy Evans <andrewevans0@outlook.com>
2016-12-10 11:13:52 +0000

    changes made for requesting mapping

--- src/main/java/hello/QuoteController.java
+++ src/main/java/hello/QuoteController.java
@@ -5,15 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private final Quote counter = new Quote();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @RequestMapping("/Quote")
+    public Quote quote {
+        return new Quote();
     }
 }
